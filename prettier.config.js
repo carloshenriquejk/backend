a4416545cdb9f32597cbd503eec4880fdbc298b3
Carlos Henrique Cardoso <carloscardosojk@gmail.com>
2026-06-21 12:41:23 -0300

    chore: initial scaffold

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
